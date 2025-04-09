289296bc5684d3a58bd00e0a4208e5023c1351d0
Oscar <oscarodriguez56@gmail.com>
2025-04-09 13:16:03 +0200

    Dado que tempScore se utiliza para solo para lo del bucle y este empieza en uno lo declaro como m_score1 desde el principio y luego simplemente añado que se hagan cambios a score si es más de uno

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,20 @@ public class Main {
      */
     public static String getScore(int m_score1, int m_score2) {
         String score = "";
-        int tempScore=0;
-
+        int tempScore=m_score1;
         if (m_score1 == m_score2) {
-            switch (m_score1)
-            {
-                case 0:
-                    return "Love-All";
-                case 1:
-                    return "Fifteen-All";
-                case 2:
-                   return "Thirty-All";
-                case 3:
-                    return "Forty-All";
-                default:
-                    return "Deuce";
-            }
+            return obtenerPuntuacionSiEmpate(m_score1);
         }
-        else if (m_score1 >=4 || m_score2 >=4) {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1)  return "Advantage player1";
-            else if (minusResult ==-1)  return "Advantage player2";
-            else if (minusResult>=2) return "Win for player1";
+        else if (isAdvantage(m_score1, m_score2)) {
+            int pointDifferences = m_score1-m_score2;
+            if (pointDifferences==1)  return "Advantage player1";
+            else if (pointDifferences ==-1)  return "Advantage player2";
+            else if (pointDifferences>=2) return "Win for player1";
             else return "Win for player2";
         }
         else {
             for (int i=1; i<3; i++) {
-                if (i==1) tempScore = m_score1;
-                else {
+                if (i>1) {
                     score+="-"; tempScore = m_score2;
                 }
                 switch(tempScore) {
@@ -52,5 +38,25 @@ public class Main {
 
         }
     return score;
+    }
+
+    private static String obtenerPuntuacionSiEmpate(int m_score1) {
+        switch (m_score1)
+        {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private static boolean isAdvantage(int m_score1, int m_score2) {
+        return m_score1 >= 4 || m_score2 >= 4;
     }
 }
