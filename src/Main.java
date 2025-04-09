c7b2b3fc566de18e84cc96182f326357fe491054
Oscar <oscarodriguez56@gmail.com>
2025-04-09 13:09:16 +0200

    Cambiando else if para añadir el retorno de la ventaja

--- src/Main.java
+++ src/Main.java
@@ -19,25 +19,22 @@ public class Main {
                     return "Forty-All";
                 default:
                     return "Deuce";
-
             }
         }
-        else if (m_score1 >=4 || m_score2 >=4)
-        {
+        else if (m_score1 >=4 || m_score2 >=4) {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1)  return "Advantage player1";
+            else if (minusResult ==-1)  return "Advantage player2";
+            else if (minusResult>=2) return "Win for player1";
+            else return "Win for player2";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
+        else {
+            for (int i=1; i<3; i++) {
                 if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+                else {
+                    score+="-"; tempScore = m_score2;
+                }
+                switch(tempScore) {
                     case 0:
                         score+="Love";
                         break;
